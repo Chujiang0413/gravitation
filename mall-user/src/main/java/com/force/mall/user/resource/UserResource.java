983fc1bc4cd6c69f865cf7cd1fdc57936c90ded5
褚江江 <chujiang0413@163.com>
2019-07-03 20:38:26 +0800

    测试Attribute

--- mall-user/src/main/java/com/force/mall/user/resource/UserResource.java
+++ mall-user/src/main/java/com/force/mall/user/resource/UserResource.java
@@ -4,9 +4,11 @@ import com.force.mall.user.pojo.EmployeeInfo;
 import com.force.mall.user.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,13 +21,20 @@ public class UserResource {
     @RequestMapping("/checkup")
     @ResponseBody
     public String checkup(){
-        return "successfully!";
+        return "successfully!!!!!!";
     }
 
     @RequestMapping("/index")
     public String index(){
         return "index";
     }
+
+    @RequestMapping("/templates")
+    public String httpRequest(HttpServletRequest httpServletRequest){
+        httpServletRequest.setAttribute("key","Hello World!");
+        return "index1";
+    }
+
 
     @RequestMapping("/getEmployeeList")
     @ResponseBody
